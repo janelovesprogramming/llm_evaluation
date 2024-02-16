e7363b80aa32c35f5cd8dd7698107b39d76cf478
janelovesprogramming <35342454+janelovesprogramming@users.noreply.github.com>
2024-02-16 18:36:31 +0300

    Add datasets, script and other files

---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+    Глобальный конфиг для всех модулей проекта.
+"""
+PATH = 'datasets/golden_dataset.csv'
+OUTPUT_FILE = 'datasets/dataset_with_metrics_.csv'
+OUTPUT_FILE_STAT = 'datasets/mean_stat.csv'
+MODEL = "all-MiniLM-L6-v2"
+LANG = "en"
